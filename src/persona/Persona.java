4b7ce4d8e563a61e2b5e503cd4891e830b007e46
vina <vina@VIAWSE02LC0427.duoc.acad>
2024-08-26 21:35:12 -0400

    primer programa

---
+++ src/persona/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package persona;
+
+/**
+ *
+ * @author vina
+ */
+public class Persona {
+
+
+    }
+    
+}
